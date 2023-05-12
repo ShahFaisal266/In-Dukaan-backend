4c1148f17c775c009898c7e7475aab8ab448d865
Shah Faisal <111347808+ShahFaisal266@users.noreply.github.com>
2023-05-12 05:36:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const Stripe = require("Stripe");
+const Stripe = require("stripe");
 const mongoose = require("mongoose");
 const axios = require("axios")
 
